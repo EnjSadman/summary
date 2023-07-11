944cecb612fd9ba71ef4829ddf3c1b0f42abbf42
Roman Pavlenko <roman.pavlenko@qbex.io>
2023-07-11 22:05:29 +0300

    added links in header

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import './Heading.scss';
 import { Link } from "react-router-dom";
 
 export const Heading = () => {
   return(
     <div className="heading">
       <nav className="heading__navigation">
-        <Link className="heading__navigation--link" to="/">Home</Link>
-        <Link className="heading__navigation--link" to="/123">123</Link>
+        <Link className="heading__navigation--link" to="/">
+          Home
+        </Link>
+        <Link className="heading__navigation--link" to="/portfolio">
+          Portfolio
+        </Link>
+        <a className="heading__navigation--link linkedin" href="https://www.linkedin.com/in/roman-pavlenko-664674242/" target="_blank" rel="noreferrer">
+          LinkedIn
+        </a>
+        <a className="heading__navigation--link github" href="https://github.com/EnjSadman" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
       </nav>
     </div>
   )
